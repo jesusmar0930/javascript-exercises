101c91efa7c6e92844cf25c596b2c698b60d6530
jesusmar0930 <35968818+youarenotevencool@users.noreply.github.com>
2024-06-08 14:54:38 -0700

    completed 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    values.forEach (value => {
+    let index;
+    while ((index = arr.indexOf(value)) !== -1){
+        arr.splice(index, 1)
+    }
+});
+    return arr;
 };
 
 // Do not edit below this line
