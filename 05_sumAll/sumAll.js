4e6310318b1d132cae797a6d0eefcd041172c698
jesusmar <35968818+youarenotevencool@users.noreply.github.com>
2024-06-08 18:48:35 -0700

    completed up to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
 
+    if (num1 > num2){
+        [num1, num2] = [num2, num1];
+    }
+    else if (num1 < 0  || num2 < 0){return "ERROR"} 
+    else if (typeof num1 !== "number"  || typeof num2 !== "number" ){return "ERROR"} 
+
+
+
+    for (let i = num1; i <= num2; i++){
+        finalSum += i
+    }
+    return finalSum;
+ 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+/*
+('sums numbers within the range', () => {
+    expect(sumAll(1, 4)).toEqual(10);
+
+    
+*/
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsiusResult;
+  celsiusResult = ((fahr - 32) * 5/9);
+  let roundedcelsiusResult = Math.round(celsiusResult * 10) / 10
+  return roundedcelsiusResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahrenheitResult; 
+  fahrenheitResult = ((9/5 * cel) + 32)
+  let roundedfahrenheitResult = Math.round(fahrenheitResult * 10) / 10
+  return roundedfahrenheitResult
+
 };
 
 // Do not edit below this line
